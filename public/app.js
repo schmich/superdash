ea5dd2efe66636879cef57f4041f8f6e722573e0
Chris Schmich <schmch@gmail.com>
2014-11-13 17:33:47 -0600

    Add ANSI colorization to logs.

--- public/app.js
+++ public/app.js
@@ -10,6 +10,92 @@ var app = angular.module('superdash', ['angular.filter']);
 // Implement stop all/start all
 // Pri 2: Group pivot
 
+// See http://misc.flogisoft.com/bash/tip_colors_and_formatting
+var colorize = function(str) {
+  var ansi = {
+    '0':  'ansi-reset',
+    '1':  'ansi-bold',
+    '2':  'ansi-dim',
+    '4':  'ansi-underline',
+    '5':  'ansi-blink',
+    '7':  'ansi-invert',
+    '8':  'ansi-hidden',
+
+    // Foreground (normal)
+    '30': 'ansi-fg-black',
+    '31': 'ansi-fg-red',
+    '32': 'ansi-fg-green',
+    '33': 'ansi-fg-yellow',
+    '34': 'ansi-fg-blue',
+    '35': 'ansi-fg-magenta',
+    '36': 'ansi-fg-cyan',
+    '37': 'ansi-fg-lgray',
+    '39': 'ansi-fg-default',
+
+    // Background (normal)
+    '40': 'ansi-bg-black',
+    '41': 'ansi-bg-red',
+    '42': 'ansi-bg-green',
+    '43': 'ansi-bg-yellow',
+    '44': 'ansi-bg-blue',
+    '45': 'ansi-bg-magenta',
+    '46': 'ansi-bg-cyan',
+    '47': 'ansi-bg-lgray',
+
+    // Foreground (light)
+    '90': 'ansi-fg-gray',
+    '91': 'ansi-fg-lred',
+    '92': 'ansi-fg-lgreen',
+    '93': 'ansi-fg-lyellow',
+    '94': 'ansi-fg-lblue',
+    '95': 'ansi-fg-lmagenta',
+    '96': 'ansi-fg-lcyan',
+    '97': 'ansi-fg-white',
+
+    // Background (light)
+    '100': 'ansi-bg-gray',
+    '101': 'ansi-bg-lred',
+    '102': 'ansi-bg-lgreen',
+    '103': 'ansi-bg-lyellow',
+    '104': 'ansi-bg-lblue',
+    '105': 'ansi-bg-lmagenta',
+    '106': 'ansi-bg-lcyan',
+    '107': 'ansi-bg-white'
+  };
+
+  str = str.replace(/\033\[((3|4|9|10)(\d))m(.*?)(?=(\033\[(0|((3|4|9|10)(\d)))m)|$)/g, function(match, p1, p2, p3, p4) {
+    return '<span class="' + ansi[p1] + '">' + p4 + '</span>';
+  });
+
+  str = str.replace(/\033\[1m(.*?)(?=((\033\[(0|21)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-bold">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[2m(.*?)(?=((\033\[(0|22)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-dim">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[4m(.*?)(?=((\033\[(0|24)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-underline">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[5m(.*?)(?=((\033\[(0|25)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-blink">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[7m(.*?)(?=((\033\[(0|27)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-invert">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[8m(.*?)(?=((\033\[(0|28)m)|$))/g, function(match, p1) {
+    return '<span class="ansi-hidden">' + p1 + '</span>';
+  });
+
+  str = str.replace(/\033\[(0|21|22|24|25|27|28)m/g, '');
+
+  return str;
+};
+
 var states = {
   stopped: 0,
   starting: 10,
@@ -91,7 +177,7 @@ app.controller('HostCtrl', function($scope, $http) {
   };
 });
 
-app.controller('ProcessCtrl', function($scope, $http) {
+app.controller('ProcessCtrl', function($scope, $http, $sce) {
   $scope.states = states;
 
   $scope.control = function(process, command) {
@@ -110,6 +196,21 @@ app.controller('ProcessCtrl', function($scope, $http) {
       });
   };
 
+  function escapeHtml(string) {
+    var entities = {
+      "&": "&amp;",
+      "<": "&lt;",
+      ">": "&gt;",
+      '"': '&quot;',
+      "'": '&#39;',
+      "/": '&#x2F;'
+    };
+
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entities[s];
+    });
+  }
+
   function updateLog(process) {
     var params = {
       group: process.group,
@@ -118,7 +219,8 @@ app.controller('ProcessCtrl', function($scope, $http) {
 
     $http.get('/hosts/' + $scope.host.id + '/process/log', { params: params })
       .success(function(res) {
-        process.log = res.log.replace(/(^\s*)|(\s*$)/g, '');
+        var log = colorize(escapeHtml(res.log.replace(/(^\s*)|(\s*$)/g, '')));
+        process.log = $sce.trustAsHtml(log);
       });
 
     if (process.watchLog) { 
